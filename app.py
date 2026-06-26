10a2ecdc99f0c798175a01ccd340555511713366
Rizwana <pu0219295@gmail.com>
2026-06-26 10:02:18 +0530

    Updated Docker setup and fixed deployment issues

--- app.py
+++ app.py
@@ -916,8 +916,10 @@ def edit_bill(bill_id):
         return redirect(f'/billing-history/{bill.patient_id}')
 
     return render_template('edit_bill.html', bill=bill)
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
